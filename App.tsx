a4c33db721f706ad510b93fa03bb21deecc1392b
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-06 01:39:13 -0300

    Box Object Model Practice

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {ContadorScreen} from './src/screens/ContadorScreen';
+import {SafeAreaView} from 'react-native';
+// import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
+import {DimensionesScreen} from './src/screens/DimensionesScreen';
+// import {ContadorScreen} from './src/screens/ContadorScreen';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 
 const App = () => {
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView style={{flex: 1}}>
+      {/* <HolaMundoScreen /> */}
+      {/* <ContadorScreen /> */}
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionesScreen />
+    </SafeAreaView>
   );
 };
 
---
+++ src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const BoxObjectModelScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Box Object Model</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    paddingHorizontal: 100,
+    paddingVertical: 20,
+    marginHorizontal: 20,
+    marginTop: 20,
+    fontSize: 20,
+    // width: 150,
+    borderWidth: 10,
+    // backgroundColor: 'red',
+  },
+});
